0caa330c17f58166f8f7a6d4e489329f7c8da92a
Timur <timur@mail.ru>
2020-02-09 17:19:47 +0400

    my first commit

---
+++ src/Hellowourd.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Arrays;
+public class Hellowourd {
+
+    public static void main(String[] args) {
+        String[] users = new String[2];
+        users[0] = "Max";
+        users[1] = "John";
+        System.out.println("Hello, " + Arrays.toString(users));
+    }
+}
